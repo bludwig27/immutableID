a596e5559199202ffd358527d634356c385d07bb
Benjamin Ludwig <ludwig22@mail.nmc.edu>
2023-03-06 12:27:10 -0500

    Add project files.

---
+++ immutableID/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace immutableID
+{
+    class Student
+    {
+        // auto-implemented properties
+        public int Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // default constructor
+        public Student()
+        {
+            Id = 0;
+            FirstName = "";
+            LastName = "";
+        }
+        // parameterized constructor
+        public Student(int i, string First, string Last)
+        {
+            Id = i;
+            FirstName = First;
+            LastName = Last;
+        }
+        //new parameterized constructor
+        public Student(int i)
+        {
+            Id = i;
+            FirstName = "";
+            LastName = "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student lisa = new Student(1);
+            lisa.FirstName = "Lisa";
+            lisa.LastName = "Simpson";
+
+            Student todd = new Student(2,"Todd","Flanders");
+
+            Console.WriteLine($"ID = {lisa.Id} Name = {lisa.FirstName} {lisa.LastName}");
+            Console.WriteLine($"ID = {todd.Id} Name = {todd.FirstName} {todd.LastName}");
+        }
+    }
+}
